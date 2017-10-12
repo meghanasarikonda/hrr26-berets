ec8dace8c303bbfc5e7c763053219fbafc32a565
Kent Shepard <kentshepard7@gmail.com>
2017-10-11 21:00:10 -0400

    Typo

--- server/api-requests.js
+++ server/api-requests.js
@@ -1,12 +1,12 @@
 const redis = require('redis');
-const var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+const client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 client.on('error', (err) => {
-  console.log('Err ---> ',err);
-})
+  console.log('Err ---> ', err);
+});
 
 client.on('connect', function() {
-    console.log('connected');
+  console.log('connected');
 });
 
 
