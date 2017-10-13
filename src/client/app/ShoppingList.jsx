4aa6f06c75592aedc8af6a7d2e9a6a37efecce07
Kent Shepard <kentshepard7@gmail.com>
2017-10-12 22:39:18 -0400

    For some reason this change didn't get merged in my other PR

--- src/client/app/ShoppingList.jsx
+++ src/client/app/ShoppingList.jsx
@@ -83,7 +83,7 @@ export default class ShoppingList extends Component {
               (this.state.renaming)
                 ?
                 <h3>
-                  <input autoFocus onFocus={this.moveCursorToEnd} onBlur={this.cancelRename} className="wish-list-edit" onChange={(e) => this.handleName(e.target.value)} type="text" value={this.props.currentListName} onKeyPress={this.handleEnterKeyPress} />
+                  <input autoFocus onFocus={this.moveCursorToEnd} onBlur={this.cancelRename} className="wish-list-edit" onChange={(e) => this.handleName(e.target.value)} type="text" defaultValue={this.props.currentListName} onKeyPress={this.handleEnterKeyPress} />
                 </h3>
                 :
                 <h3>
