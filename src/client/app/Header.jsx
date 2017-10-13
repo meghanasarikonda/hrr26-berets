7edc8369e82b64e4d823ab13557245bf981809ef
Kent Shepard <kentshepard7@gmail.com>
2017-10-13 01:25:22 -0400

    Change search from form to a div

--- src/client/app/Header.jsx
+++ src/client/app/Header.jsx
@@ -29,10 +29,10 @@ class Header extends React.Component {
                     <a className="nav-link" href="#">Log In</a>
                   </li>
                 </ul>
-                <form className="form-inline my-2 my-lg-0">
+                <div className="form-inline my-2 my-lg-0">
                   <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
                   <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                </div>
               </div>
             </div>
           </nav>
