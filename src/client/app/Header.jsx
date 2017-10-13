5a35e5613dca48d122443eed257a23810042ab25
Kent Shepard <kentshepard7@gmail.com>
2017-10-13 01:20:34 -0400

    Put navbar content inside a container

--- src/client/app/Header.jsx
+++ src/client/app/Header.jsx
@@ -15,23 +15,25 @@ class Header extends React.Component {
       return (
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">wishList</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-right navbar-collapse" id="navbarText">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Sign Up</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Log In</a>
-                </li>
-              </ul>
-              <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-              </form>
+            <div className="container">
+              <a className="navbar-brand" href="#">wishList</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-right navbar-collapse" id="navbarText">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Sign Up</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Log In</a>
+                  </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                  <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+              </div>
             </div>
           </nav>
         </div>
